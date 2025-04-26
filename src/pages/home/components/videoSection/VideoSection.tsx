f599df93827721d431336e8bcbfd51bf89af8b09
sevabrov <vsevolod.brovarnyi@gmail.com>
2025-04-26 10:01:28 +0200

    [TC-19] Add correct progam description + fix feedback slider

--- src/pages/home/components/videoSection/VideoSection.tsx
+++ src/pages/home/components/videoSection/VideoSection.tsx
@@ -1,4 +1,3 @@
-import flower1 from 'assets/flowers/flower1.webp';
 import './index.scss';
 import YouTubeEmbed from './Video';
 
@@ -10,8 +9,7 @@ const VideoSection = () => {
           <h2>Що це за курс?</h2>
           <hr />
           <p>
-            Переглянь відео (2хв. 11с.) і дізнайся усі деталі які тебе
-            чекатимуть на курсі!
+            Переглянь це відео та дізнайся усі деталі, які тебе чекатимуть на курсі!
           </p>
         </header>
         <div className='VideoSection_video'>
@@ -23,21 +21,6 @@ const VideoSection = () => {
           <hr />
           <button className='btn btn-primary'>купити курс</button>
         </footer>
-        <picture className='VideoSection_decor-image'>
-          <img
-            alt=''
-            data-nimg='1'
-            decoding='async'
-            height='1584'
-            loading='lazy'
-            src={flower1}
-            srcSet={flower1}
-            style={{
-              color: 'transparent',
-            }}
-            width='1174'
-          />
-        </picture>
       </div>
     </section>
   );
